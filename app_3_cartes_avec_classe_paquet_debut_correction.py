4166ef31e86f24c1979926f0f00cd327b26a3628
yzpt <yzpt.de@gmail.com>
2025-04-15 14:34:44 +0200

    ajout fichier début de correction app_3 cartes avec classe paquet

--- app_3_cartes_avec_classe_paquet_debut_correction.py
+++ app_3_cartes_avec_classe_paquet_debut_correction.py
@@ -13,46 +13,28 @@ class Paquet:
         self.cartes = []  # liste de cartes vide
 
     def ajouter_carte(self, carte):
-        valeur = input('Taper la valeur de la carte à ajouter :')
-        couleur = input('Taper la couleur de la carte à ajouter :')
-        carte = Carte(valeur, couleur)
         self.cartes.append(carte)
         print(f"Carte ajoutée : {carte.valeur} de {carte.couleur}")
         input("Appuyez sur Entrée pour continuer...")
         
     def afficher_cartes(self):
-        ...
+        clear_console()
+        print("Liste des cartes :")
+        for carte in self.cartes:
+            carte.afficher()
+        input("Appuyez sur Entrée pour revenir au menu...")
 
-    def melanger(self):
-        ...
-
-    def tirer_carte(self):
-        ...
-
-    def retirer_carte(self, carte):
-        ...
-
-    def vider_paquet(self):
-        ...
-
+# ---------------------
+# Interface application
+# ---------------------
 
 def clear_console():
     print("\n" * 50)
 
 def afficher_menu():
     print("Bienvenue dans votre collection de cartes")
-    print("1. Afficher toutes les cartes qu paquet")
+    print("1. Afficher toutes les cartes du paquet")
     print("2. Ajouter une carte")
-    print("3. Mélanger les cartes")
-    print("4. Tirer une carte du paquet, l'afficher, la remettre") # attention à vérifier si le paquet est vide !
-    print("5. Retirer une carte du paquet") # attention à vérifier si le paquet est vide !
-    print("6. Vider le paquet")
-    
-    print("7. Ajouter automatiquement les 52 cartes à jouer dans paquet") 
-    # option7: facultatif mais très intéressant : il faut imbriquer 2 boucles for.
-    # valeurs = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Valet", "Dame", "Roi", "As"]
-    # couleurs = ["Coeur", "Pique", "Trèfle", "Carreau"]
-    # ... que faire ensuite ?
     
     print("q. Quitter")
 
@@ -73,20 +55,16 @@ if __name__ == "__main__":
         choix = input("Faites votre choix : ").strip().lower()
         
         if choix == "1":
-            ...
-            
-        elif choix == "2":
-            ...
-            
-        elif choix == "3":
-            ...
+            # Afficher toutes les cartes
+            paquet.afficher_cartes()
         
-        elif choix == "4":
-            ...
+        elif choix == "2":
+            # Ajouter une carte
+            valeur = input("Entrez la valeur de la carte : ")
+            couleur = input("Entrez la couleur de la carte : ")
+            nouvelle_carte = Carte(valeur, couleur)
+            paquet.ajouter_carte(nouvelle_carte)                        
             
-        elif choix == '5':
-            ...
-                        
         elif choix == "q":
             clear_console()
             print("À bientôt !")
